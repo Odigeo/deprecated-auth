03258f9e07c0db865aba828d04a02c81caeb3b41
Peter Bengtson <peter@peterbengtson.com>
2013-06-30 12:53:02 +0200

    More Rails 4 changes.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,10 +12,6 @@ Auth::Application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=3600"
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -24,7 +20,7 @@ Auth::Application.configure do
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment
-  config.action_controller.allow_forgery_protection    = false
+  config.action_controller.allow_forgery_protection = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
