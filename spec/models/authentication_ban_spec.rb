e801477d498a5df7ef5af0ac349f84ddfb648c08
Peter Bengtson <peter@peterbengtson.com>
2013-07-13 00:39:40 +0200

    Added Authentication DELETE specs for BAN.

---
+++ spec/models/authentication_ban_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+
+describe Authentication do
+
+  before :each do
+    stub_const "LOAD_BALANCERS", ["127.0.0.1"]
+  end
+
+
+  it "should have an empty varnish_invalidate_member list" do
+    Authentication.varnish_invalidate_member.length.should == 0
+  end
+
+  it "should have an empty varnish_invalidate_member list" do
+    Authentication.varnish_invalidate_member.length.should == 0
+  end
+
+
+  it "should trigger no BANs when created" do
+    Api.should_not_receive(:call_p).with("http://127.0.0.1", :ban, "/v[0-9]+/authentications/TheToken")
+  	create :authentication, token: "TheToken"
+  end
+
+
+  it "should trigger no BANs when updated" do
+    Api.stub(:call_p)
+    m = create :authentication
+    Api.should_not_receive(:call_p).with("http://127.0.0.1", :ban, "/v[0-9]+/authentications/#{m.token}")
+    m.token = "Zalagadoola"
+ 	  m.save!
+  end
+
+
+  it "should trigger no BANs when touched" do
+    Api.stub(:call_p)
+    m = create :authentication
+    Api.should_not_receive(:call_p).with("http://127.0.0.1", :ban, "/v[0-9]+/authentications/#{m.token}")
+ 	  m.touch
+  end
+
+
+  it "should trigger one BANs when destroyed" do
+    Api.stub(:call_p)
+    m = create :authentication
+    Api.should_not_receive(:call_p).with("http://127.0.0.1", :ban, "/v[0-9]+/authentications/#{m.token}")
+  	m.destroy
+  end
+
+end
